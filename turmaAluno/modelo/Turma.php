3d1614d376fba8d6b891c42301d9e56be49cdb5f
Gustavo <134728517+gustavoGGV@users.noreply.github.com>
2024-09-19 15:46:22 -0300

    Create Turma.php

---
+++ turmaAluno/modelo/Turma.php
@@ -0,0 +1,61 @@
+<?php
+
+class Turma {
+    private string $nome;
+    private string $curso;
+    private array $alunos;
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     */
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of curso
+     */
+    public function getCurso(): string
+    {
+        return $this->curso;
+    }
+
+    /**
+     * Set the value of curso
+     */
+    public function setCurso(string $curso): self
+    {
+        $this->curso = $curso;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of alunos
+     */
+    public function getAlunos(): array
+    {
+        return $this->alunos;
+    }
+
+    /**
+     * Set the value of alunos
+     */
+    public function setAlunos(array $alunos): self
+    {
+        $this->alunos = $alunos;
+
+        return $this;
+    }
+}
